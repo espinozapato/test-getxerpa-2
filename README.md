981b72a0f942f8dd0889191643a3c2d55adbccd2
pespinozam <espinozapato2001@gmail.com>
2023-05-21 14:11:59 -0400

    subida testing y postman collection, update readme

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# test-getxerpa-2
-Prueba técnica Django Backend Developer Getxerpa- Patricio Espinoza
+# Prueba Técnica Django Backend Developer Getxerpa- Patricio Espinoza
+
+## Instalación y Ejecución via Docker
+
+1. Clonar repositorio del proyecto
+```
+git clone https://github.com/espinozapato/test-getxerpa-2.git
+```
+2. Ejecutar Docker Compose
+```
+docker-compose up || docker compose up
+```
+3. Cargar App 
+```
+http://127.0.0.1:8000/
+```
+
+## Instalación y Ejecución Localmente
+
+1. Clonar repositorio del proyecto
+```
+git clone https://github.com/espinozapato/test-getxerpa-2.git
+```
+2. Instalar Python Version 3.x
+
+3. Instalar dependencias
+```
+pip install -r requirements.txt
+```
+4. Ejecutamos la Aplicación
+```
+python manage.py runserver
+```
+5. Cargar App 
+```
+http://127.0.0.1:8000/
+```
+
+## EndPoints App
+
+-   CRUD Categorias [GET, POST, PUT, DELETE]
+```
+/categorias/
+```
+-   CRUD Transacción [GET, POST, PUT, DELETE]
+```
+/transacciones/
+```
+
+-   Filtros Categorias 
+```
+/categorias/?filter=exceeds-limit
+```
+```
+/categorias/?filter=not-exceeds-limit
+```
+
+## Ejecutar Tests
+
+```
+python manage.py test
+```
+
+## Postman Workspace - Collection
+
+```
+test-getxerpa-2.postman_collection
+```
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,132 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+class CategoriaTestCase(APITestCase):
+
+    url = '/categorias/'
+
+    def test_get_categoria(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_categoria_filter_exceeds_limit(self):
+        url = self.url + '?filter=exceeds-limit'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_categoria_filter_not_exceeds_limit(self):
+        url = self.url + '?filter=not-exceeds-limit'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_categoria(self): 
+        response = self.client.post(self.url, 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_put_categoria(self): 
+        post = self.client.post(self.url, 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+
+        url = self.url + str(post.data["id"]) + '/'
+        response = self.client.put(url, 
+        {
+            "name": "Tecnología",
+            "limit": "2000",
+        },)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_categoria(self): 
+        post = self.client.post(self.url, 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+
+        url = self.url + str(post.data["id"]) + '/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class TransaccionTestCase(APITestCase):
+
+    url = '/transacciones/'
+
+    def test_get_transaccion(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_transaccion(self):
+        categoria = self.client.post('/categorias/', 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+
+        response = self.client.post(self.url, 
+        {
+            "description": "Falabella",
+            "amount": 350,
+            "date": "2023-05-20",
+            "ignore": False,
+            "category": categoria.data["id"],
+        },)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_put_transaccion(self):
+        categoria = self.client.post('/categorias/', 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+
+        post = self.client.post(self.url, 
+        {
+            "description": "Falabella",
+            "amount": 350,
+            "date": "2023-05-20",
+            "ignore": False,
+            "category": categoria.data["id"],
+        },)
+
+        url = self.url + str(post.data["id"]) + '/'
+
+        response = self.client.put(url, 
+        {
+            "description": "Falabella",
+            "amount": 500,
+            "date": "2023-05-20",
+            "ignore": False,
+            "category": categoria.data["id"],
+        },)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_transaccion(self):
+        categoria = self.client.post('/categorias/', 
+        {
+            "name": "Tecnología",
+            "limit": "1500",
+        },)
+
+        post = self.client.post(self.url, 
+        {
+            "description": "Falabella",
+            "amount": 350,
+            "date": "2023-05-20",
+            "ignore": False,
+            "category": categoria.data["id"],
+        },)
+
+        url = self.url + str(post.data["id"]) + '/'
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
